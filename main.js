80b37ed9833e8992384229fd82bce370c2998b32
Vishal Trivedi <vishaltrivedi92@gmail.com>
2022-02-17 13:41:03 -0600

    doing this again

---
+++ main.js
@@ -0,0 +1,17 @@
+const add = (firstNumber, secondNumber) => {
+    const sum = firstNumber + secondNumber
+
+    return sum
+}
+
+const multiply = (firstNumber) => {
+    const sum = firstNumber * 2
+
+    return sum
+}
+
+const newNumber = add(4,14)
+console.log(newNumber)
+
+const newNumber2 = multiply(18)
+console.log(newNumber2)
